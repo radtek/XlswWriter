10821729ad41a5ded231bf59bfaf267a6952e386
welkj <welkj@welkj.com>
2019-06-05 18:24:09 +0800

    [Init] 初使化版本

---
+++ README.md
@@ -0,0 +1,5 @@
+### 说明
+此项目主要是将 [libxlsxwriter](https://libxlsxwriter.github.io) 转成 iOS 动态 Framework。
+
+### 步骤
+主要是将官方说明的 Pod 版本安装完成后，提取文件后修复一下 header 的引用。
---
+++ XlswWriter/XlswWriter.h
@@ -0,0 +1,19 @@
+//
+//  XlswWriter.h
+//  XlswWriter
+//
+//  Created by welkj on 2019/6/5.
+//  Copyright © 2019 Lecason Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for XlswWriter.
+FOUNDATION_EXPORT double XlswWriterVersionNumber;
+
+//! Project version string for XlswWriter.
+FOUNDATION_EXPORT const unsigned char XlswWriterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XlswWriter/PublicHeader.h>
+
+
